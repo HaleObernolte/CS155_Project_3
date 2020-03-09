fccd56c6acaf94123faf7e22ca337634810335fd
HaleObernolte <36969737+HaleObernolte@users.noreply.github.com>
2020-03-09 11:40:44 -0700

    Added rhyme processing

--- sonnet_processing.py
+++ sonnet_processing.py
@@ -18,8 +18,8 @@ import string
 # File processing   #
 #####################
 
-# Returns a list of all sonnets in file given by f_name.
-#    In this case, each sonnet is just a list of words
+# Returns a list of all sonnets in file given by f_name, as well as the word map
+#   and list of rhyme tuples.
 def get_sonnets(f_name, max_words=4000):
     f = open(f_name, "r")
     # Skip first sonnet number
@@ -30,6 +30,7 @@ def get_sonnets(f_name, max_words=4000):
     sonnets = []
     while (obs_counter < max_words):
         sonnet = []
+        rhyme = ()
         cont = True
         while (True):
             # get line
@@ -45,7 +46,8 @@ def get_sonnets(f_name, max_words=4000):
                 f.readline()
                 break
             words = line.split()
-            for word in words:
+            for i in range(len(words)):
+                word = words[i]
                 w = word.translate(str.maketrans('', '', string.punctuation))
                 w = w.lower()
                 if w not in obs_map:
@@ -53,7 +55,7 @@ def get_sonnets(f_name, max_words=4000):
                     obs_map[w] = obs_counter
                     obs_counter += 1
                 # Add the encoded word.
-                sonnet.append(obs_map[w])
+                sonnet.append(obs_map[w])               
         sonnets.append(sonnet)
         # Check for EOF
         if (not cont):
@@ -79,4 +81,44 @@ def get_syllable_map(f_name):
         if w not in syl_map:
             syl_map[w] = n
     return syl_map
-    
+
+
+# Returns all rhyme pairs used by Shakespeare
+#
+# Each rhyme pair is repeated in the list whenever it is een so that taking a
+# random rhyme from the list is sampling from Shakespeare's distribution.
+# NOTE: Assumes you have deleted Sonnets 99 and 123
+def get_rhymes(f_name):
+    f = open(f_name, "r")
+    rhymes = []
+    line_no = 1
+    a = ()
+    b = ()
+    g = ()
+    for line in f:
+        words = line.split()
+        if (len(words) < 2):
+            continue        
+        w = words[-1]
+        w = w.translate(str.maketrans('', '', string.punctuation))
+        w = w.lower()
+        # Add to correct tuple
+        if (line_no == 1 or line_no == 3 or line_no == 5 or line_no == 7 or line_no == 9 or line_no == 11):
+            a = a + (w,)
+            if (len(a) == 2):
+                rhymes.append(a)
+                a = ()
+        elif (line_no == 2 or line_no == 4 or line_no == 6 or line_no == 8 or line_no == 10 or line_no == 12):
+            b = b + (w,)
+            if (len(b) == 2):
+                rhymes.append(b)
+                b = ()
+        elif (line_no == 13):
+            g = g + (w,)
+        else:
+            g = g + (w,)
+            rhymes.append(g)
+            g = ()
+            line_no = 0
+        line_no += 1
+    return rhymes
